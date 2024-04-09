901bdde899cba343a9d7f426f4e691ec9874c99b
bementshibeshi <150736497+bementshibeshi@users.noreply.github.com>
2024-04-09 17:52:51 -0400

    Add files via upload

---
+++ js/challenge3.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', fucntion(){
+    let figcaptions = document.querySelectorAll('figcaption');
+    let secondFigcaption = figCaption[1];
+    alert(secondFigcaption.innerHTML);
+)
